963f7a24a79eba84d99e1b505401707516a7201b
tets20 <y.s.bubko@gmail.com>
2025-04-07 12:05:22 +0300

    Ex16: Запрос данных другого пользователя

--- lib/assertions.py
+++ lib/assertions.py
@@ -23,3 +23,31 @@ class Assertions:
     def assert_code_status(response: Response, expected_status_code):
         assert response.status_code == expected_status_code, \
             f"Unexpected status code!Expected {expected_status_code}. Actual: {response.status_code} "
+
+    @staticmethod
+    def assert_json_has_not_key(response: Response, name):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format.Response text is '{response.text}'"
+        assert name not in response_as_dict, f"Response JSON dhouldn't have key'{name}, But it's present"
+
+    @staticmethod
+    def assert_json_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format.Response text is '{response.text}'"
+
+        for name in names:
+            assert name in response_as_dict, f"Response JSON doesn't have key'{name}'"
+
+    @staticmethod
+    def assert_json_not_has_keys(response: Response, names: list):
+        try:
+            response_as_dict = response.json()
+        except json.JSONDecodeError:
+            assert False, f"Response is not JSON format.Response text is '{response.text}'"
+
+        for name in names:
+            assert name not in response_as_dict, f"Response JSON have key'{name}'"
